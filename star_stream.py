ba60fb18c2dd2684fa5064ce76ef41d1491e45c1
CCCpeggy <peggy19925@gmail.com>
2021-09-19 16:40:39 +0800

    修正cog路徑與Redbot路徑不同問題

--- star_stream.py
+++ star_stream.py
@@ -547,7 +547,8 @@ class StarStream(commands.Cog):
         需求：電腦需要安裝 git bash，且這個 cog 是 git 的資料夾
         更新完畢後，需要重新 reload 這個 cog
         """
-        cmd = f"cd {os.path.dirname(os.path.abspath(__file__))} & git pull origin master"
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        cmd = f"{file_path[0]}: & cd {file_path} & git pull origin master"
         # f = os.popen(cmd, "r")
         # message = f.read()
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -565,7 +566,7 @@ class StarStream(commands.Cog):
             elif "changed" in output:
                 await ctx.send(f"已更新到 origin master 的最新版本，請使用 reload 重新載入程式。")
             elif "Already up to date" in output:
-                await ctx.send(f"已經是最新版本了。")
+                await ctx.send(f"你就已經最新了，不用更新好嗎？(月嵐 ver.)")
             else:
                 raise Exception
         except:
