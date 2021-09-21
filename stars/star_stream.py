ed259a579b9e0084b7bf141ef44e6a8ee05f250a
CCCpeggy <peggy19925@gmail.com>
2021-09-21 23:10:02 +0800

    修正 + 8 時區

--- stars/star_stream.py
+++ stars/star_stream.py
@@ -1335,7 +1335,7 @@ def getDiscordTimeStamp(date):
 
 def datetime_plus_8_to_0_isoformat(date):
     date = parse_time(date)
-    timestamp = getTimeStamp(date) - 8
+    timestamp = getTimeStamp(date) - 8 * 60 * 60
     date = datetime.fromtimestamp(timestamp)
     return date.isoformat()
 
