85f3450cc11e2ecee89d82442b4e5544872f66cf
CCC <32979547+CCCpeggy@users.noreply.github.com>
2022-05-17 12:19:05 +0800

    暫停私訊

--- membership_role/membership_role.py
+++ membership_role/membership_role.py
@@ -117,12 +117,12 @@ class MembershipRoleManger(commands.Cog):
                             continue
                         elif not await user_role.check():
                             await dc_user.remove_roles(role, reason=f"身分組到期: {user_role.end_time}")
-                            await dc_user.send(f"您的 {member.names[0]} 會員身分組已到期，請再次發送驗證資料")
+                            # await dc_user.send(f"您的 {member.names[0]} 會員身分組已到期，請再次發送驗證資料")
                         elif member.default_type_name:
                             default_role_id = member.membership_type[member.default_type_name]
                             if default_role_id != role_id:
                                 default_role = get(guild.roles, id=default_role_id)
-                                await dc_user.send(f"您的新一個月的 {member.names[0]} 會員身分組驗證成功，但因無法驗證等級，所以自動降為 {member.default_type_name}，如需提高等級請重新發送驗證資料")
+                                # await dc_user.send(f"您的新一個月的 {member.names[0]} 會員身分組驗證成功，但因無法驗證等級，所以自動降為 {member.default_type_name}，如需提高等級請重新發送驗證資料")
                                 await dc_user.remove_roles(role, reason=f"身分組到期 ({user_role.end_time})，更換成預設身分組")
                                 await dc_user.add_roles(default_role, reason=f"身分組到期 ({user_role.end_time})，更換成預設身分組")
                                 user.roles[default_role_id] = user.roles[role_id]
